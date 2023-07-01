70330a0ed6c42ee8b2246dce7b04da77e84d1e86
LightYe4r <24adrenalin@naver.com>
2023-07-02 05:28:51 +0900

    ADD : Views, urls, serializers, admin

---
+++ project/catchtable/serializers.py
@@ -0,0 +1,23 @@
+import rest_framework.serializers as serializers
+from .models import User, Restaurant, Menu, Review, Reservation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
--- project/catchtable/views.py
+++ project/catchtable/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import User, Restaurant, Menu, Review, Reservation
+from .serializers import UserSerializer, RestaurantSerializer, MenuSerializer, ReviewSerializer, ReservationSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
